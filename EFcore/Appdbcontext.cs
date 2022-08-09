b3f233d7a8233bf7326e6a9ff3ba6fec3a37257e
samy khater <samykhater55@outlook.com>
2022-08-09 18:57:13 +0200

    Add project files.

---
+++ EFcore/Appdbcontext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFcore
+{ 
+    public class Appdbcontext:DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=DESKTOP-SH8J5TV\SQLEXPRESS;Initial Catalog=forfun;Integrated Security=True");
+        }
+
+       public DbSet<emplyee> emplyees { get; set; }
+    }
+}
---
+++ EFcore/Migrations/20220809165613_addempmodel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFcore.Migrations
+{
+    public partial class addempmodel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "emplyees",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_emplyees", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "emplyees");
+        }
+    }
+}
